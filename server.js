420804fd25c2b10ea890c65ee6539c475c56f9e2
rrodriguez <rrodriguez@dude-monkey.com>
2018-10-05 17:18:12 +0200

    Correccion a GET headers

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.get('projects', (req, res) => {
+app.get('/projects', (req, res) => {
     res.render('projects.hbs',{
         pageTitetle: 'Projects' 
     });
