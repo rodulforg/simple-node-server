b4cb5bb9bab73d32950997f5380196d696bdf485
rrodriguez <rrodriguez@dude-monkey.com>
2018-10-05 17:14:44 +0200

    Projects file added

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('projects', (req, res) => {
+    res.render('projects.hbs',{
+        pageTitetle: 'Projects' 
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fulfil request'
